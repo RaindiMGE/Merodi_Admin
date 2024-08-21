bdc967095e555122583af75f118d37203184b26c
RaindiMGE <mgeladze.raindi06@gmail.com>
2024-08-21 14:42:06 +0400

    fix: Fix Conflict

--- app/Components/InputComponent/InputComponent.tsx
+++ app/Components/InputComponent/InputComponent.tsx
@@ -1,21 +1,21 @@
 'use client'
 
-import { FieldError, UseFormRegisterReturn } from 'react-hook-form';
 import styles from './InputComponent.module.scss';
+import { FieldError, UseFormRegisterReturn } from 'react-hook-form';
 import { useState } from 'react';
 import Image from 'next/image';
 
 interface Props {
     placeholder: string;
-    forAdminPassword?: boolean;
-    error?: FieldError | undefined;
-    register?: UseFormRegisterReturn;
+    isAdmin?: boolean;
+    error: FieldError;
+    register: UseFormRegisterReturn;
     isLoginInput?: boolean;
 }
 
 const Input = (props: Props) => {
 
-    const [inputType, setInputType] = useState(props.forAdminPassword ? 'password' : 'text')
+    const [inputType, setInputType] = useState(props.isAdmin ? 'password' : 'text')
 
     const onClick = () => {
         if (inputType == 'text') setInputType('password')
@@ -23,14 +23,11 @@ const Input = (props: Props) => {
     }
 
     return <div className={styles.container}>
-        <div className={styles.inputWrapper} style={{
+        <div className={props.isLoginInput ? styles.loginInputWrapper : styles.inputWrapper} style={{
             borderColor: props.error ? '#FF3333' : '',
-            border: props.isLoginInput ? '0' : '',
-            padding: props.isLoginInput ? '12px 0' : '',
-            borderBottom: props.isLoginInput ? '1px solid #E3E5E8' : '',
         }}>
             <input type={'email'} placeholder={props.placeholder} {...props.register} className={styles.input} />
-            {props.forAdminPassword && <Image src={'/icons/defaultShowPassword.svg'} alt='show password' width={24} height={24} onClick={onClick} />}
+            {props.isAdmin && <Image src={'/icons/defaultShowPassword.svg'} alt='show password' width={24} height={24} onClick={onClick} />}
         </div>
         {props.error && <span className={styles.error}>{props.error.message}</span>}
     </div>
