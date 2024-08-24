46b03db609ec7fc7573cad0558bb7cf4b9a7b3c5
RaindiMGE <mgeladze.raindi06@gmail.com>
2024-08-24 16:28:24 +0400

    feat/Create Block And Delete Pop-ups

--- app/Components/Buttons/SecondaryButton/SecondaryButton.tsx
+++ app/Components/Buttons/SecondaryButton/SecondaryButton.tsx
@@ -6,10 +6,16 @@ interface Props {
     title: string;
     isBlue?: boolean;
     disabled?: boolean;
+    onClick?: () => void;
 }
 
 const SecondaryButton = (props: Props) => {
-    return <button disabled={props.disabled} type='button' className={`${styles.button} ${props.isBlue ? styles.blueButton : styles.grayButton}`}>
+    return <button
+        disabled={props.disabled}
+        type='button'
+        className={`${styles.button} ${props.isBlue ? styles.blueButton : styles.grayButton}`}
+        onClick={props.onClick}
+    >
         {props.title}
     </button>
 }
