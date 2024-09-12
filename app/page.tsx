dd04d74e94dcda87ee22eeeb0267215d7fce08a9
ramazikotavadze <ramaziko.tavadze.98@icloud.com>
2024-09-12 16:15:00 +0400

    orgin/feat/CHAL-75/createpassword

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 
+import ChangePassword from './Components/ChangePassword/ChangePassword';
 import styles from './page.module.scss';
 
 export default function Home() {
   return (
     <main>
-      
+      <ChangePassword userId={'change password'}/>
     </main>
   );
 }
