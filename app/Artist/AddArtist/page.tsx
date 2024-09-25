cfe8c4ba5cc0aa6b808cf6af4bcc58c228996a56
ramazikotavadze <ramaziko.tavadze.98@icloud.com>
2024-09-25 17:08:04 +0400

    add artist

---
+++ app/Artist/AddArtist/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import AddInfoModel from "@/app/Components/AddInfoModel/AddInfoModel";
+
+
+
+const AddArtist = () => {
+    return <div>
+        <AddInfoModel onCancelClick={()=>{}}/>
+      
+
+    </div>
+}
+
+
+
+export default AddArtist
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 'use client'
 
 
-import ChangePassword from './Components/ChangePassword/ChangePassword';
 import styles from './page.module.scss';
 
 export default function Home() {
   return (
     <main>
-      <ChangePassword userId={'change password'}/>
+     
     </main>
   );
 }
